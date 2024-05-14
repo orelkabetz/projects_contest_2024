5caee470b7dcc731e2db7906afb318e0c8d27747
eor <eor@videocites.com>
2024-05-14 11:10:24 +0300

    Update App.js

--- mta-final-projects-site/src/App.js
+++ mta-final-projects-site/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
-import AdminHome from './admin-homepage'; // Add this import
+// import AdminHome from './admin-homepage'; // Add this import
 import JudgeHome from './users/judge/JudgeHome'; // Add this import
 import ProfileSetup from './users/judge/ProfileSetup';
 import ViewProjects from './users/judge/ViewProjects';
@@ -22,7 +22,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/judge-homepage" element={<JudgeHome />} /> // Update this route 
+          <Route path="/judge" element={<JudgeHome />} /> // Update this route 
           <Route path="/profile-setup" element={<ProfileSetup />} /> // Update this route 
           <Route path="/view-projects" element={<ViewProjects />} /> // Update this route 
           <Route path="/grade-projects" element={<GradeProjects />} /> // Update this route          
