60ad4be668dcf1e33f804d1dd0f30de29b8e54a7
eor <eor@videocites.com>
2024-06-15 13:33:08 +0300

    Added projects search & filter

--- mta-final-projects-site-backend-server/Routers/users/admin.router.js
+++ mta-final-projects-site-backend-server/Routers/users/admin.router.js
@@ -61,16 +61,49 @@ getCollections()
 
   router.get('/projects/projectsList', async (req, res) => {
     try {
-      const projects = await collections.project_schemas.find({}, { 
-        projection: { name: 1, Title: 1, WorkshopName: 1, WorkshopId: 1, ProjectOwners: 1, Lecturer: 1,
-          StudentName: 1, StudentEmail: 1, StudentPhone:1} }).toArray();
-      res.json(projects);
+        const { search, searchField } = req.query;
+        let query = {};
+
+        if (search && searchField) {
+            // Construct the query dynamically based on the selected searchField
+            query = {
+                [searchField]: { $regex: search, $options: 'i' }
+            };
+        } else if (search) {
+            // Fallback to the existing logic if only search term is provided without a specific field
+            query = {
+                $or: [
+                    { name: { $regex: search, $options: 'i' } },
+                    { Title: { $regex: search, $options: 'i' } },
+                    { WorkshopName: { $regex: search, $options: 'i' } },
+                    { ProjectOwners: { $regex: search, $options: 'i' } },
+                    { Lecturer: { $regex: search, $options: 'i' } },
+                    { StudentName: { $regex: search, $options: 'i' } },
+                    { StudentEmail: { $regex: search, $options: 'i' } },
+                    { StudentPhone: { $regex: search, $options: 'i' } }
+                ]
+            };
+        }
+
+        const projects = await collections.project_schemas.find(query, {
+            projection: {
+                name: 1,
+                Title: 1,
+                WorkshopName: 1,
+                WorkshopId: 1,
+                ProjectOwners: 1,
+                Lecturer: 1,
+                StudentName: 1,
+                StudentEmail: 1,
+                StudentPhone: 1
+            }
+        }).toArray();
+        res.json(projects);
     } catch (error) {
-      console.error('Error fetching projects:', error);
-      res.status(500).json({ error: 'An error occurred while fetching projcets' });
+        console.error('Error fetching projects:', error);
+        res.status(500).json({ error: 'An error occurred while fetching projects' });
     }
   });
-    
   })
   .catch((err) => {
     console.error('Error getting collections:', err);
